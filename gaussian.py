1b7ec4d2363dbaba73c9c6f48c9a760b180f6d58
Lawrence Tray <ltray99@gmail.com>
2019-11-11 15:49:08 +0000

    3F3 Add sigma control

--- gaussian.py
+++ gaussian.py
@@ -4,22 +4,37 @@ from ksdensity import ksdensity
 from pdfs import n_pdf
 
 N = 1000
+sigma = 0.4
 bins = 30
 
+img_dir = "/mnt/c/Users/ltray/Documents/Cambridge/3F3/img/"
+
 # Plot normal distribution
-fig = plt.figure()
 x = np.random.randn(1000)  # randn is standard normal distribution
 x_values = np.linspace(-5., 5., 100)
 
 ks_density = ksdensity(x, width=0.4)
 scale = lambda el: el * N / bins
 
+fig = plt.figure()
 
-plt.title('Histogram of normal distribution (N={})'.format(N))
+plt.title('Histogram of gaussian distribution (N={})'.format(N))
 plt.hist(x, bins=bins, density=True, label="normalised frequency")
 plt.plot(x_values, n_pdf(x_values), linestyle='dashed', label="gaussian pdf")
 plt.xlabel('x')
 plt.ylabel('Normalised frequency')
 plt.legend()
 
+plt.savefig(img_dir + "gaussian_hist.png")
+
+fig = plt.figure()
+plt.title('Kernel smoothed gaussian distribution (N={}, sigma={})'.format(N, sigma))
+ks_density = ksdensity(x, width=sigma)
+plt.plot(x_values, ks_density(x_values) / sigma, label="kernel smoothed")
+plt.plot(x_values, n_pdf(x_values), linestyle='dashed', label="gaussian pdf")
+plt.xlabel('x')
+plt.ylabel('probability density')
+plt.legend()
+
+plt.savefig(img_dir + "gaussian_ksd.png")
 plt.show()
--- uniform.py
+++ uniform.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ksdensity import ksdensity
 from pdfs import uniform_pdf
-import os
 
 N = 1000
+sigma = 0.2
 bins = 30
 
 img_dir = "/mnt/c/Users/ltray/Documents/Cambridge/3F3/img/"
@@ -12,9 +12,6 @@ img_dir = "/mnt/c/Users/ltray/Documents/Cambridge/3F3/img/"
 # Plot normal distribution
 x = np.random.rand(1000)  # uniform dist [0,1]
 x_values = np.linspace(-0.2, 1.2, 1000)
-
-ks_density = ksdensity(x, width=0.4)
-scale = lambda el: el * N / bins
 
 fig = plt.figure()
 plt.title('Histogram of uniform distribution (N={})'.format(N))
@@ -27,10 +24,10 @@ plt.legend()
 plt.savefig(img_dir + "uniform_hist.png")
 
 fig = plt.figure()
-plt.title('Kernel smoothed uniform distribution')
-ks_density = ksdensity(x, width=0.2)
+plt.title('Kernel smoothed uniform distribution (N={}, sigma={})'.format(N, sigma))
+ks_density = ksdensity(x, width=sigma)
 x_values = np.linspace(-1., 2., 100)
-plt.plot(x_values, ks_density(x_values) * 5, label="kernel smoothed")
+plt.plot(x_values, ks_density(x_values) / sigma, label="kernel smoothed")
 plt.plot(x_values, uniform_pdf(x_values), linestyle='dashed', label="uniform pdf")
 plt.xlabel('x')
 plt.ylabel('probability density')
